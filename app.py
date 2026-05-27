0d701cb4b66cc27c8dbf44201d071f4e7bdd323d
Alenlen1 <alenandreiamarante0313@gmail.com>
2026-05-27 09:42:57 +0800

    change mailport to 465

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mail = Mail(app)
 # ── Gmail SMTP CONFIG ─────────────────────
 app.config.update(
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_PORT=587,
+    MAIL_PORT=465,
     MAIL_USE_TLS=True,
     MAIL_USE_SSL=False,
     MAIL_USERNAME=os.environ.get("MAIL_USERNAME"),
